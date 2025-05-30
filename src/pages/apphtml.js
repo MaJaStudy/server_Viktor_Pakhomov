f417e2411efbfd8142ed84b588be7ee260a57f39
Fraise <nevergonnagiveyouupnevergonn4@gmail.com>
2025-05-31 10:59:47 +1100

    Fix
    Пофикшена динамическая генерация контента в body, слегка обновлён Readme

--- src/pages/apphtml.js
+++ src/pages/apphtml.js
@@ -1,4 +1,36 @@
-return
+/*return
 `<h1>Пахомов Виктор Васильевич №231</h1>
-<li><a href="lab1\\mockup.html">Calculator</a></li>
-<li><a href="lab2\\mockup.html">ToDo</a></li>`
+
+/* document.body.onload = addElement();
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("Place");
+  document.getElementById("Place").appendChild(newDiv)
+} */
+/*document.body.onload = function() {
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("Hi there and greetings!");
+    newDiv.appendChild(newContent);
+    document.getElementById("Place").appendChild(newDiv)
+} */
+document.addEventListener('DOMContentLoaded',function(){
+    const header = document.getElementById('Place')
+
+    const info = {
+        SN: 'Пахомов Виктор Васильевич',
+        group: '№231'
+    }
+
+    const infoElement = document.createElement('h1')
+    infoElement.textContent = `${info.SN} ${info.group}`
+    header.appendChild(infoElement)
+})
