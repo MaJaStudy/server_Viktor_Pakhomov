23abe8900388dc1f29eda6a751cca7d18a304211
MaJaNames <42037799+MaJaNames@users.noreply.github.com>
2025-05-31 22:37:59 +1100

    Removed comments

--- src/pages/apphtml.js
+++ src/pages/apphtml.js
@@ -1,27 +1,3 @@
-/*return
-`<h1>Пахомов Виктор Васильевич №231</h1>
-
-/* document.body.onload = addElement();
-function addElement() {
-  // create a new div element
-  const newDiv = document.createElement("div");
-
-  // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
-
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("Place");
-  document.getElementById("Place").appendChild(newDiv)
-} */
-/*document.body.onload = function() {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Hi there and greetings!");
-    newDiv.appendChild(newContent);
-    document.getElementById("Place").appendChild(newDiv)
-} */
 document.addEventListener('DOMContentLoaded',function(){
     const header = document.getElementById('Place')
 
