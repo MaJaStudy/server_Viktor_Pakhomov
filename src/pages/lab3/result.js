b9f1a9f83ed7958876da051b7181101653f8c6a4
MaJaNames <42037799+MaJaNames@users.noreply.github.com>
2025-06-11 18:01:00 +1100

    Added 3rd lab.

---
+++ src/pages/lab3/result.js
@@ -0,0 +1,12 @@
+document.getElementById('knpk').addEventListener('click', async () =>{
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then((req) => {return req.json()})
+    .then(data => {
+        const res = Object.entries(data.Valute)
+        .map(([code, currency]) => `"${code}": ${currency.Value} руб.`).join('\n')
+        document.getElementById('out').textContent = res 
+    })
+    .catch(err =>{
+        document.getElementById('out').textContent = `Ошибка: ${err.message}`
+    })
+})
